23831b21478bfcfd8490f0d8f114c4bd857cf3c7
LiviaStyles <100550047+LiviaStyles@users.noreply.github.com>
2022-03-05 17:51:33 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
+//dynamically update rate value
+function updateRate()
+{
+  var rateval = document.getElementById("rate").value;
+  document.getElementById("rate_val").innerText=rateval;
+}
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    //validate number greater than 0
+  let x = document.getElementById("principal").value;
+  let text;
+  if (isNaN(x) || x < 1 ) {
+    alert("Enter a positive number")
+  } else {
+      //return results
+    var principal = document.getElementById("principal").value;
+
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var year = new Date().getFullYear()+parseInt(years);
+    var interest = principal * years * rate /100;
+
+        document.getElementById("result").innerHTML = "\<br\>\<br\>If you deposit <span class='highlight'>" + principal + "</span>,\<br\>at an interest rate of <span class='highlight'>" + rate + "%</span>\<br\>You will receive an amount of <span class='highlight'>" + interest + "</span>,\<br\>in the year <span class='highlight'>" + year + "</span>.";
 }
-        
+
+}
